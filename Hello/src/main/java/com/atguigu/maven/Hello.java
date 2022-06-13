6b5cb58e536e22b745be81cf49d71db7288dc2af
lm19960309 <1443282510@qq.com>
2022-06-13 09:42:11 +0800

    Version1.1

--- Hello/src/main/java/com/atguigu/maven/Hello.java
+++ Hello/src/main/java/com/atguigu/maven/Hello.java
@@ -1,7 +1,12 @@
 package com.atguigu.maven;
 
 public class Hello {
-    public String sayHello(String name){
+    public String sayHello(String name) {
         return "Hello" + name + "!";
+    }
+
+
+    public void VcTest() {
+        System.out.println("Version1.1");
     }
 }
